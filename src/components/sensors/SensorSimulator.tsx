1fb31f8a6df6d9946ac4241a8ff12a7b472d7e3f
Robert Philipp <rob.philipp@gmail.com>
2020-11-23 08:32:32 -0500

    updated gitignore

---
+++ src/components/sensors/SensorSimulator.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import { useState } from 'react';
+
+interface Props {
+    sensorDescription: string;
+}
+
+function SensorSimulator(props: Props): JSX.Element {
+    const {
+        sensorDescription
+    } = props;
+
+
+    return <div/>
+}
